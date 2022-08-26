df23a743e22c06da9800443027fe17b4fa1770a1
DivijaBabu <divijababu@piktorlabs.com>
2022-08-26 17:25:01 +0530

    projeccts

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const overviewcontainer = document.querySelector('.overviewcontainer');
-   const scroller = overviewcontainer.querySelector('.scroller');
-   const nextBtn = overviewcontainer.querySelector('.btn.next');
-   const prevBtn = overviewcontainer.querySelector('.btn.prev');
-   const itemWidth = overviewcontainer.querySelector('.overview-card').clientWidth;
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+        const activeslide = slides.querySelector("[data-active]")
+        let newindex = [...slides.children].indexOf(activeslide) + offset
+        if (newindex < 0)
+            newindex = slides.children.length - 1
+        if (newindex >= slides.children.length)
+            newindex = 0
+        slides.children[newindex].dataset.active = true
+        delete activeslide.dataset.active
+    })
+})
  
-   nextBtn.addEventListener('click', scrollToNextItem);
-   prevBtn.addEventListener('click', scrollToPrevItem);
- 
-   function scrollToNextItem() {
-       scroller.scrollBy({left: itemWidth, top: 0, behavior: 'smooth'});
-   }
-   function scrollToPrevItem() {
-       scroller.scrollBy({left: -itemWidth, top: 0, behavior: 'smooth'});
-   }
-
-
-
-   
