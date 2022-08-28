2362d0cd4fd9f4cf0ca5f29f4332fd9d9ccc649d
DivijaBabu <divijababu@piktorlabs.com>
2022-08-28 21:09:25 +0530

    alldone

--- index.js
+++ index.js
@@ -81,5 +81,34 @@ function arrows(direction){
 }
 
 
+function searchshipment() {
+let input = document.getElementById('searchbar').value
+input=input.toLowerCase();
+let x = document.getElementsByClassName('card');
+
+for (i = 0; i < x.length; i++) {
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display="none";
+    }
+    else {
+        x[i].style.display="bottomcards";             
+    }
+}
+}
+
+function searchshipmentmobile() {
+  let input = document.getElementById('searchmobile').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('card');
+  
+  for (i = 0; i < x.length; i++) {
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="bottomcards";             
+      }
+  }
+  }
 
 
